4a23bd3f8833b48ec4955746063d14329b83c29b
richardboyz <r1382333@gmail.com>
2022-12-17 00:56:44 +0800

    Add fetch quiz method in api provider

--- lib/resources/api_provider.dart
+++ lib/resources/api_provider.dart
@@ -1,6 +1,17 @@
 import 'package:dio/dio.dart';
+import 'package:quiz_game/models/quiz_model.dart';
 
 class ApiProvider {
   final Dio _dio = Dio();
   final String _url = 'https://opentdb.com/api.php?amount=1';
+
+  Future<QuizModel> fetchQuiz() async {
+    try {
+      Response response = await _dio.get(_url);
+      return QuizModel.fromJson(response.data['results'][0]);
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return QuizModel.withError("Data not found / Connection issue");
+    }
+  }
 }
