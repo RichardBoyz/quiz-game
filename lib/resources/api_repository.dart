69d58cf5f9f7496f440e5459e5bf024086483841
richardboyz <r1382333@gmail.com>
2022-12-17 00:58:35 +0800

    Add fetch quiz method in api-repository

--- lib/resources/api_repository.dart
+++ lib/resources/api_repository.dart
@@ -1,3 +1,11 @@
-class ApiRepository {}
+import 'package:quiz_game/models/quiz_model.dart';
+import 'package:quiz_game/resources/api_provider.dart';
+
+class ApiRepository {
+  final _provider = ApiProvider();
+  Future<QuizModel> fetchQuiz() {
+    return _provider.fetchQuiz();
+  }
+}
 
 class NetworkError extends Error {}
