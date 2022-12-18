56d91ea38023c76369c72d95d13d72f936ec6866
richardboyz <r1382333@gmail.com>
2022-12-19 00:30:47 +0800

    Set quiz page layout

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quiz_game/blocs/quiz_bloc/quiz_bloc.dart';
+import 'package:quiz_game/blocs/timer_bloc/timer_bloc.dart';
+import 'package:quiz_game/models/quiz_model.dart';
+import 'package:quiz_game/models/ticker.dart';
 
 class QuizPage extends StatefulWidget {
   const QuizPage({super.key});
@@ -8,11 +13,57 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  final QuizBloc _quizBloc = QuizBloc();
+  final TimerBloc _timer = TimerBloc(ticker: const Ticker());
+
+  @override
+  void initState() {
+    _quizBloc.add(GetQuiz());
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: BlocProvider(
+        create: (context) => _timer,
+        child: BlocProvider(
+          create: (context) => _quizBloc,
+          child: BlocListener<QuizBloc, QuizState>(
+            listener: (context, state) {
+              if (state is QuizError) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(state.message!),
+                  ),
+                );
+              }
+            },
+            child: BlocBuilder<QuizBloc, QuizState>(
+              builder: (context, state) {
+                if (state is QuizInitial) {
+                  return _buildLoading();
+                } else if (state is QuizLoading) {
+                  return _buildLoading();
+                } else if (state is QuizLoaded) {
+                  return _buildQuestion(context, state.quizModel);
+                } else if (state is QuizError) {
+                  return Container();
+                } else {
+                  return Container();
+                }
+              },
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  Widget _buildLoading() => const Center(child: CircularProgressIndicator());
+
+  Widget _buildQuestion(BuildContext context, QuizModel model) {
+    return Container();
   }
 }
