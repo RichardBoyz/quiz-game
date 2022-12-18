51c5737c03297d17673fd5aad01a3b5cc49ae07e
richardboyz <r1382333@gmail.com>
2022-12-19 00:26:11 +0800

    Add quiz page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_game/pages/quiz_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Quiz Game'),
+      routes: {'quiz-page': ((context) => const QuizPage())},
     );
   }
 }
