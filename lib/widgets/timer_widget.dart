4c68d29a450fa19a6adbaf387917d085145546fe
richardboyz <r1382333@gmail.com>
2022-12-19 00:44:20 +0800

    Complete timer_widget layout

--- lib/widgets/timer_widget.dart
+++ lib/widgets/timer_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quiz_game/blocs/timer_bloc/timer_bloc.dart';
 
 class TimerWidget extends StatelessWidget {
   // const TimerWidget({super.key});
@@ -7,6 +9,32 @@ class TimerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    if (context.read<TimerBloc>().state is TimerRunComplete) {
+      onComplete();
+    }
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Stack(
+        children: [
+          Center(
+            child: SizedBox(
+              width: 30,
+              height: 30,
+              child: CircularProgressIndicator(
+                value:
+                    (context.select((TimerBloc bloc) => bloc.state.duration) /
+                            15)
+                        .toDouble(),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              '${context.select((TimerBloc bloc) => bloc.state.duration)}',
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
