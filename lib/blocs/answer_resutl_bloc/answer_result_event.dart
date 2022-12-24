caa4fc1c936dc664ad0018011ddbe16ade77cbd1
richardboyz <r1382333@gmail.com>
2022-12-24 20:54:01 +0800

    Set answer-result event

--- lib/blocs/answer_resutl_bloc/answer_result_event.dart
+++ lib/blocs/answer_resutl_bloc/answer_result_event.dart
@@ -6,3 +6,34 @@ abstract class AnswerResultEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class LoadAnswerResult extends AnswerResultEvent {
+  final AnswerResult answerResult;
+  const LoadAnswerResult(
+      {this.answerResult = const AnswerResult(
+        canSelect: true,
+        correctCount: 0,
+        incorrectCount: 0,
+        isCorrectChoice: false,
+        isSelected: false,
+        selectedChoice: '',
+      )});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [answerResult];
+}
+
+class UpdateAnswerResult extends AnswerResultEvent {
+  final AnswerResult answerResult;
+  const UpdateAnswerResult(this.answerResult);
+}
+
+class ResetAnswerResultExcludeCount extends AnswerResultEvent {
+  final AnswerResult answerResult;
+  const ResetAnswerResultExcludeCount(this.answerResult);
+}
+
+class ResetAnswerResult extends AnswerResultEvent {
+  const ResetAnswerResult();
+}
